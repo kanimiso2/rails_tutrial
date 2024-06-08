bbc18d8298984638b378108595ec2f8220910656
kanimiso2 <111118183+kanimiso2@users.noreply.github.com>
2024-06-08 09:40:37 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'static_pages/home'
+  #get 'users/new'
+  #get 'static_pages/home'
   
   
   root "static_pages#home"
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get  "/signup",  to: "users#new"
+  resources :users
 end
